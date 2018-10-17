92968df4722fcd1d3cd6ba5e8edb9cc5a2c57779
Bartosz Mylka <bmylka@gmail.com>
2018-10-17 20:49:26 +0200

    added simple function makeFormAppear + p tag

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 var start = new Date().getTime();
 
+function makeFormAppear(){
+    
+    document.getElementById("form").style.display = "block";
+    
+    start = new Date().getTime();
+    
+}
+
 document.getElementById("form").onclick = function() {
     
     document.getElementById("form").style.display = "none";
     
     var end = new Date().getTime();
     
-    var duration = end - start;
+    var duration = (end - start) / 1000;
     
-    alert(duration);
-    
+    document.getElementById("time").innerHTML = duration + "s";
 }

aea2d68948ac0ae83bc39321dba5d4f98dfd9f02
Bartosz Mylka <bmylka@gmail.com>
2018-10-21 12:40:42 +0200

    added random color function

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 var start = new Date().getTime();
 
+function getRandomColor() {
+      var letters = '0123456789ABCDEF';
+      var color = '#';
+      for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+}
+
 function makeFormAppear(){
     
     var top = Math.random() * 400;
@@ -7,6 +16,8 @@ function makeFormAppear(){
     var left = Math.random() * 400;
     
     var size = Math.random() * 200;
+    
+    document.getElementById("form").style.backgroundColor = getRandomColor();
     
     document.getElementById("form").style.top = top + "px";
     
